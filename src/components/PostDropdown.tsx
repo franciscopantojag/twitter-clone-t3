a37e2178cc7d0060ab21e7c73b3a8de59e28d857
Francisco Pantoja <franciscopantojag98@gmail.com>
2023-05-01 16:07:59 -0500

    fix: hover effect only on mouse devices

--- src/components/PostDropdown.tsx
+++ src/components/PostDropdown.tsx
@@ -1,80 +1,79 @@
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
+import styles from './styles.module.scss';
 
 const PostDropdown = ({ onDelete }: { onDelete: () => void }) => (
   <Menu as="div" className="relative inline-block h-full text-left">
-    <div className="h-full">
-      <Menu.Button
-        className="inline-flex w-full justify-center rounded-full text-sm font-medium
-      text-gray-500
-      hover:bg-gray-500
-      hover:text-white
-      hover:ring-2
-      hover:ring-gray-500
-      focus:outline-none
-      focus-visible:text-white
-      focus-visible:ring-2
-      focus-visible:ring-gray-500
-      "
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-          />
-        </svg>
-      </Menu.Button>
-    </div>
-    <Transition
-      as={Fragment}
-      enter="transition ease-out duration-100"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-    >
-      <Menu.Items
-        style={{
-          boxShadow:
-            'rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px',
-        }}
-        className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-black font-semibold shadow-lg focus:outline-none"
-      >
-        <div>
-          <Menu.Item>
-            <button
-              className="group flex w-full items-center px-2 py-2 text-sm text-red-500"
-              onClick={onDelete}
+    {({ open }) => (
+      <>
+        <div className="h-full">
+          <Menu.Button
+            className={`inline-flex w-full justify-center rounded-full text-sm font-medium${
+              open ? ' text-white' : ' text-gray-500'
+            } focus:outline-none${
+              styles.correctHover ? ` ${styles.correctHover}` : ''
+            }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="mr-2 h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-              Delete
-            </button>
-          </Menu.Item>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </Menu.Button>
         </div>
-      </Menu.Items>
-    </Transition>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items
+            style={{
+              boxShadow:
+                'rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px',
+            }}
+            className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-black font-semibold shadow-lg focus:outline-none"
+          >
+            <div>
+              <Menu.Item>
+                <button
+                  className="group flex w-full items-center px-2 py-2 text-sm text-red-500"
+                  onClick={onDelete}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="mr-2 h-5 w-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                  Delete
+                </button>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </>
+    )}
   </Menu>
 );
 export default PostDropdown;
